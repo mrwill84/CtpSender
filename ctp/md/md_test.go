5ee3ac504b1967e56ac53753c4f2f0186529146f
mrwill84 <zhao.qing@grabtaxi.com>
2017-05-23 14:40:57 +0800

    F

--- ctp/md/md_test.go
+++ ctp/md/md_test.go
@@ -2,7 +2,10 @@ package md
 
 import (
 	"fmt"
+	"log"
 	"testing"
+
+	"github.com/mrwill84/goctp"
 )
 
 func TestMapGochan(t *testing.T) {
@@ -13,4 +16,33 @@ func TestMapGochan(t *testing.T) {
 	delete(all, 0)
 	c <- "etet"
 	fmt.Println(<-c)
+}
+
+func TestMdLogin(t *testing.T) {
+
+	CTP = GoCTPClient{
+		BrokerID:   "0",
+		InvestorID: "2000052",
+		Password:   "276988",
+
+		MdFront: "tcp://101.230.8.33:41213",
+		MdApi:   goctp.CThostFtdcMdApiCreateFtdcMdApi(),
+		//TraderFront: *trade_front,
+		//TraderApi:   goctp.CThostFtdcTraderApiCreateFtdcTraderApi(),
+		MdRequestID:     0,
+		TraderRequestID: 0,
+	}
+
+	log.Printf("客户端配置: %+#v\n", CTP)
+
+	go func() {
+		pMdSpi := goctp.NewDirectorCThostFtdcMdSpi(&GoCThostFtdcMdSpi{Client: CTP})
+
+		CTP.MdApi.RegisterSpi(pMdSpi)
+		CTP.MdApi.RegisterFront(CTP.MdFront)
+		CTP.MdApi.Init()
+
+		CTP.MdApi.Join()
+		CTP.MdApi.Release()
+	}()
 }
