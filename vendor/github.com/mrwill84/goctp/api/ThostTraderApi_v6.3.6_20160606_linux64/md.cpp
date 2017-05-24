7bf10d02af68b6fc43f6019d43f952f532d9fb4f
mrwill84 <zhao.qing@grabtaxi.com>
2017-05-24 23:46:22 +0800

    sub

--- vendor/github.com/mrwill84/goctp/api/ThostTraderApi_v6.3.6_20160606_linux64/md.cpp
+++ vendor/github.com/mrwill84/goctp/api/ThostTraderApi_v6.3.6_20160606_linux64/md.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include "ThostFtdcMdApi.h"
 
+const char* context[] ={
+    "rb1701",
+    "rb1701"
+}
+
 class GoMdSpi: public CThostFtdcMdSpi{
     public:
         GoMdSpi(){
@@ -8,12 +13,15 @@ class GoMdSpi: public CThostFtdcMdSpi{
         }
         virtual void OnFrontConnected(){
             std::cout<<"OnFrontConnected" <<std::endl;
+            m_api->SubscribeMarketData(context)
         }
+    private:
+        CThostFtdcMdApi * m_api;
 };
 int main(){
     GoMdSpi* pSpi = new GoMdSpi();
     std::cout<<"hello world"<<std::endl;
-    CThostFtdcMdApi * api = CThostFtdcMdApi::CreateFtdcMdApi("");
+    CThostFtdcMdApi * api = CThostFtdcMdApi::CreateFtdcMdApi();
     api->RegisterSpi(pSpi);
 	api->RegisterFront((char*)"tcp://101.230.8.33:41213");
 	api->Init();
