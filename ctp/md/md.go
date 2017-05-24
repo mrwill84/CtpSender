e4defc46391ecb44f981a705769fa6b81e7073eb
mrwill84 <zhao.qing@grabtaxi.com>
2017-05-24 12:52:11 +0800

    fix

--- ctp/md/md.go
+++ ctp/md/md.go
@@ -2,8 +2,10 @@ package md
 
 import (
 	"flag"
+	"fmt"
 	"log"
 
+	"github.com/mrwill84/CtpSender/ctp/async"
 	"github.com/mrwill84/goctp"
 )
 
@@ -78,4 +80,33 @@ func (p *GoCThostFtdcMdSpi) IsErrorRspInfo(pRspInfo goctp.CThostFtdcRspInfoField
 		log.Printf("ErrorID=%v ErrorMsg=%v\n", pRspInfo.GetErrorID(), pRspInfo.GetErrorMsg())
 	}
 	return bResult
+}
+
+func (p *GoCThostFtdcMdSpi) ReqUserLogin() (interface{}, error) {
+	id := p.Client.GetMdRequestID()
+	obj, err := async.Alloc(id, func() {
+		log.Println("GoCThostFtdcMdSpi.ReqUserLogin.")
+		req := goctp.NewCThostFtdcReqUserLoginField()
+		req.SetBrokerID(p.Client.BrokerID)
+		req.SetUserID(p.Client.InvestorID)
+		req.SetPassword(p.Client.Password)
+		requestID := p.Client.GetMdRequestID()
+		iResult := p.Client.MdApi.ReqUserLogin(req, p.Client.GetMdRequestID())
+
+		if iResult == 0 {
+			log.Println("发送用户登录请求: 成功.")
+			return
+		}
+		log.Println("发送用户登录请求: 失败.")
+	})
+	return obj, err
+}
+
+func (p *GoCThostFtdcMdSpi) OnRspUserLogin(pRspUserLogin goctp.CThostFtdcRspUserLoginField, pRspInfo goctp.CThostFtdcRspInfoField, nRequestID int, bIsLast bool) {
+
+	if bIsLast && !p.IsErrorRspInfo(pRspInfo) {
+		async.Put(nRequestID, pRspUserLogin, nil)
+		return
+	}
+	async.Put(nRequestID, nil, fmt.Errorf("pRspUserLogin error "))
 }
