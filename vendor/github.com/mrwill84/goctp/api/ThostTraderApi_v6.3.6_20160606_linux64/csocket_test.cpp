7a9555f30cfd290d9e432f5b9aac7ce1feeb3392
mrwill84 <zhao.qing@grabtaxi.com>
2017-05-26 12:27:43 +0800

    f

--- vendor/github.com/mrwill84/goctp/api/ThostTraderApi_v6.3.6_20160606_linux64/csocket_test.cpp
+++ vendor/github.com/mrwill84/goctp/api/ThostTraderApi_v6.3.6_20160606_linux64/csocket_test.cpp
@@ -1,11 +1,17 @@
+#include <string>
+#include <iostream>
 #include "csocket.h"
 int main(){
     int sockdf=create_socket();
     if (sockdf!=-1){
+        std::cout<<"create_socket"<<std::endl;
+
         bool ret = connect_to(sockdf, "10.10.0.1",21234);
+        std::cout<<"connect_to: "<<ret <<std::endl;
         if (ret == true) {
             const char* buf="test" ;
-            send_to(sockdf,buf,5);
+            int rc = send_to(sockdf,buf,5);
+            std::cout<<"send_to: "<<rc <<std::endl;
         }
     }
     return 0;
