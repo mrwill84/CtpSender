1018c15c05708c7a8030f33142338d1ff47dc5d0
mrwill84 <zhao.qing@grabtaxi.com>
2017-05-27 15:41:26 +0800

    f

--- vendor/github.com/mrwill84/goctp/api/ThostTraderApi_v6.3.6_20160606_linux64/timecov_test.cpp
+++ vendor/github.com/mrwill84/goctp/api/ThostTraderApi_v6.3.6_20160606_linux64/timecov_test.cpp
@@ -10,10 +10,10 @@ int main(){
     assert(recv == "2009-09-01 12:13:38");
     std::uint64_t i = UINT64_C(1251807218000000450);
     std::tm t;
-    std::istringstream ss(input);
+    std::istringstream ss(recv);
     ss >> std::get_time(&t, "%Y-%m-%D %H:%M:%S");
     time_t t1 =utc_mktime(&t)
     auto tp = std::chrono::system_clock::from_time_t(std::mktime(&tm));
-    uint64 unixnano = std::chrono::duration_cast<std::chrono::nano>(tp.time_since_epoch()).count())
+    std::uint64_t unixnano = std::chrono::duration_cast<std::chrono::nano>(tp.time_since_epoch()).count())
     return i == unixnano;
 }
