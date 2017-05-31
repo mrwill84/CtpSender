22b4488d20d5f9f268ce828d2a07f6792637a49c
mrwill84 <zhao.qing@grabtaxi.com>
2017-05-31 10:04:17 +0800

    f

--- vendor/github.com/mrwill84/goctp/api/ThostTraderApi_v6.3.6_20160606_linux64/timecov_test.cpp
+++ vendor/github.com/mrwill84/goctp/api/ThostTraderApi_v6.3.6_20160606_linux64/timecov_test.cpp
@@ -14,8 +14,8 @@ int main(){
     std::uint64_t i = UINT64_C(1251807218000000450);
     std::tm t;
     std::istringstream ss(recv);
-    ss >> std::get_time(&t, "%Y-%m-%D %H:%M:%S");
-    time_t t1 =utc_mktime(&t);
+    ss >> std::get_time(&t, "%Y-%m-%d %H:%M:%S");
+    //time_t t1 =utc_mktime(&t);
     // std::cout<<"utc_mktime: " <<t1 <<std::endl;
     auto tp = std::chrono::system_clock::from_time_t(std::mktime(&t));
     std::uint64_t unixnano = std::chrono::duration_cast<std::chrono::nanoseconds>(tp.time_since_epoch()).count();
