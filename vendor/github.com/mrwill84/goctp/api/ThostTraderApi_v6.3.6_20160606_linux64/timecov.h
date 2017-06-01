cee8e67b40756f4752ccb7bc698c5d4b2f667801
mrwill84 <zhao.qing@grabtaxi.com>
2017-06-01 11:35:01 +0800

    f

--- vendor/github.com/mrwill84/goctp/api/ThostTraderApi_v6.3.6_20160606_linux64/timecov.h
+++ vendor/github.com/mrwill84/goctp/api/ThostTraderApi_v6.3.6_20160606_linux64/timecov.h
@@ -12,7 +12,7 @@ std::string format_time(const std::string& date, const std::string& time){
 
 std::uint64_t utc_maketimesmape(const std::string& recv,int millic){
     std::tm t;
-    std::strptime(recv.c_str(),"%Y%m%d %H:%M:%S", &t);
+    strptime(recv.c_str(),"%Y%m%d %H:%M:%S", &t);
     //std::istringstream ss(recv);
     //ss >> std::get_time(&t, "%Y%m%d %H:%M:%S");
     auto tp = std::chrono::system_clock::from_time_t(std::mktime(&t)+28800);
